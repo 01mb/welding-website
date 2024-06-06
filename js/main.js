ea15bfb06d9b4e513c997ad6e1b4d2b6539a3787
01mb <91267011+01mb@users.noreply.github.com>
2024-06-06 16:38:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,10 +36,18 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 100) {
+    //         $('.back-to-top').fadeIn('slow');
+    //     } else {
+    //         $('.back-to-top').fadeOut('slow');
+    //     }
+    // });
+    // $('.back-to-top').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // });
 
 
     // Portfolio isotope and filter
